969314b45e9e4d096a7c1beba13944ae001b24ce
ming <wang_ming@iclould.com>
2020-02-15 18:03:25 +0800

    add build-env

--- docker/test.sh
+++ docker/test.sh
@@ -4,6 +4,6 @@ docker build -t dockerfile_example .
 
 i=0
 time dgoss run dockerfile_example || ((i++))
-time dgoss run -e DEBUG=true dockerfile_example || ((i++))
+#time dgoss run -e DEBUG=true dockerfile_example || ((i++))
 
 exit $i
